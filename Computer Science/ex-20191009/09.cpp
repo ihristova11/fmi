3eeeacd0ef7ae8361a2d0a8a5a3cb2504ba5cb1f
Irina Hristova <hristova119@gmail.com>
2019-10-10 20:02:17 +0300

    feat: cpp sln to 09.

---
+++ Computer Science/ex-20191009/09.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <iomanip>
+
+int main()
+{
+	int l = 0;
+	bool res = true;
+
+	std::cin >> l;
+
+	do
+	{
+		if (l % 2 != 0)
+		{
+			res = false;
+			break;
+		}
+		l /= 2;
+	} while (l != 2);
+
+	std::cout << res << std::endl;
+	return 0;
+}
