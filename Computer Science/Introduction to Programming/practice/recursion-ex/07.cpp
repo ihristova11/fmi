a6d2656f8a8b403dae4e4172538b0ae8fa5ddd45
Irina Hristova <hristova119@gmail.com>
2019-12-17 19:25:08 +0200

    feat: recursion ex

---
+++ Computer Science/Introduction to Programming/practice/recursion-ex/07.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int fib(int n)
+{
+	int min;
+	if (n >= 1 && n <= 2)
+	{
+		return 1;
+	}
+
+	return fib(n - 1) + fib(n - 2);
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	cout << fib(n) << endl;
+
+	return 0;
+}
